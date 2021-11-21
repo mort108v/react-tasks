7e853e8641ab06b2d1337d7f1117a12af9f53ed0
Morten Six <mort108v@stud.kea.dk>
2021-11-21 13:27:37 +0100

    Removed unused vars: Link and Outlet from import in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
